5559380d99bee37f226926a805e24b92e3eddf97
RadhikaSNM <radsnm@gmail.com>
2015-09-18 15:18:04 -0500

    Login and Preferences updation
    1) Stubs created for webserver
    2) json changes
    3) Login: new user and returning user
    4) preferences : not logged in, new user, returning user

--- ARHome/app/src/main/java/com/wikitude/virtualhome/Login.java
+++ ARHome/app/src/main/java/com/wikitude/virtualhome/Login.java
@@ -29,6 +29,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.app.ActionBar;
 
+import com.wikitude.tools.activities.MediaPlayerActivity;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -51,6 +53,8 @@ public class Login extends Activity {
     private boolean newUserFlag;
     JSONObject userDetailsJson;
     private boolean creationNotSuccess=false;
+    private boolean LoginSuccess=false;
+
 
     public static final String PREFERENCES_Gallery_FILE_NAME = "VHGalleryPreferences";
 
@@ -138,12 +142,10 @@ public class Login extends Activity {
             createUserJson();
         }
 
-        Intent intent = new Intent(this, Preferences.class);
+    /*    Intent intent = new Intent(this, Preferences.class);
         //For now.
-        //TODO: DElete
-        intent.putExtra("email", email);
         //TODO: Put this on postExecute???
-        startActivity(intent);
+        startActivity(intent);*/
     }
 
 
@@ -162,6 +164,8 @@ public class Login extends Activity {
             userDetailsJson.put("password", pwd);
             userDetailsJson.put("newUserFlag", newUserFlag);*/
 
+            Log.i("Login","create json user");
+
             //Using the below so as to match the format on server side: Needs to be changed
             userDetailsJson.put("name", "adad");
             userDetailsJson.put("email", email);
@@ -175,7 +179,18 @@ public class Login extends Activity {
 
         //Post to the server
 
-        new PostAsynTask().execute();
+        //Depending on the flow.(whether new user or not)
+
+        if (newUserFlag) {
+            //Call the new user creation rest api
+            new PostAsynTask().execute();
+        }
+        else
+        {
+            //Call the user validation rest api
+        }
+
+
 
 
     }
@@ -249,7 +264,7 @@ public class Login extends Activity {
                     JSONObject jsonObject = null;
                     String userID = null;
                     try {
-                        Log.i("LoginAsync ", "Retrieve jsonContent");
+                        Log.i("New User Async ", "Retrieve jsonContent");
 
                         //System.out.println("inside : " + serverSB);
 
@@ -271,7 +286,6 @@ public class Login extends Activity {
                             SharedPreferences settings = getSharedPreferences(PREFERENCES_Gallery_FILE_NAME, 0);
 
                             //Initially for cross checking
-
                             System.out.println(settings.getString("user_id", "0"));
                             System.out.println(settings.getString("email", "0"));
                             System.out.println(settings.getString("password", "0"));
@@ -282,15 +296,19 @@ public class Login extends Activity {
                             editor.putString("email", email);
                             editor.putString("password", pwd);
 
-                            editor.commit();
-                            //For confirmation
 
+
+                            editor.commit();
+
+                            //For confirmation
                             System.out.println(settings.getString("user_id", "0"));
                             System.out.println(settings.getString("email", "0"));
                             System.out.println(settings.getString("password", "0"));
 
                             //Save the preferences data
                             //Get the data and save the preferences.
+
+
 
 
                         }
@@ -319,6 +337,221 @@ public class Login extends Activity {
             return "";
 
         }
+
+
+
+
+        protected void onPostExecute(String result) {
+            Log.i("VirtualHome-Login", "onPostExecute-Create User");
+
+            //TODO: check the creation success and then navigate
+            if(newUserFlag) {
+                Toast.makeText(getApplicationContext(), "Welcome!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Login.this, Preferences.class);
+                intent.putExtra("newUserFlag","true");
+                startActivity(intent);
+            }
+
+
+        }
     }
+
+    private class LoginAsynTask extends AsyncTask<String, String, String> {
+        protected String doInBackground(String... arg0) {
+            HttpURLConnection urlConnection = null;
+
+            //String url1= "http://ec2-54-219-182-125.us-west-1.compute.amazonaws.com:8080/api/v1/users";
+            String url1= "http://ec2-54-215-226-210.us-west-1.compute.amazonaws.com:8080/api/v4/login";
+
+            StringBuilder sb = new StringBuilder();
+            try {
+
+                URL url = new URL(url1);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setUseCaches(false);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+
+                //urlConnection.setRequestProperty("Host", "android.schoolportal.gr");
+                urlConnection.connect();
+
+                OutputStreamWriter out = new OutputStreamWriter(urlConnection.getOutputStream());
+                out.write(userDetailsJson.toString());
+                out.flush();
+                out.close();
+
+
+                String responseMessage=urlConnection.getResponseMessage();
+                int HttpResult =urlConnection.getResponseCode();
+                System.out.println("CODE: "+HttpResult);
+
+
+                //TODO: check for created or check for validated. If not throw pop up as error.
+                System.out.println(responseMessage);
+
+                //TODO: this should be different status code
+                if (urlConnection.getResponseCode() != HttpURLConnection.HTTP_ACCEPTED) {
+
+                    Log.i("Login", "---Failed : HTTP error code : "
+                            + urlConnection.getResponseCode());
+                    LoginSuccess=false;
+
+                    /*throw new RuntimeException("Failed : HTTP error code : "
+                            + urlConnection.getResponseCode());*/
+
+
+                }
+                else {
+                    //It is verified:
+                    LoginSuccess=true;
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(
+                            (urlConnection.getInputStream())));
+
+                    String serverOutput;
+                    StringBuilder serverSB = new StringBuilder();
+                    System.out.println("Output from Server .... \n");
+                    while ((serverOutput = br.readLine()) != null) {
+
+                        System.out.println(serverOutput);
+                        serverSB.append(serverOutput + "\n");
+
+                    }
+
+                    //Parsing the returned json
+                    JSONObject jsonObject = null;
+                    String userID = null;
+                    String gender= null;
+                    String family= null;
+                    String profession= null;
+                    String gardening= null;
+                    String interiorDesign= null;
+                    String cooking= null;
+                    String painting= null;
+                    String music= null;
+                    String reading= null;
+
+
+                    try {
+                        Log.i("LoginAsync ", "Retrieve jsonContent");
+
+                        //System.out.println("inside : " + serverSB);
+
+                        jsonObject = new JSONObject(serverSB.toString());
+                        //Assuming only 1 jsonObject is returned.
+                        if (jsonObject != null) {
+                            //Obtaining the data
+                            userID = jsonObject.getString("user_id");
+                            gender = jsonObject.getString("gender");
+                            family = jsonObject.getString("family");
+                            profession = jsonObject.getString("profession");
+                            interiorDesign = jsonObject.getString("interiorDesign");
+                            cooking = jsonObject.getString("cooking");
+                            painting = jsonObject.getString("painting");
+                            reading = jsonObject.getString("reading");
+                            music = jsonObject.getString("music");
+
+
+                            //Printing the whole json data obtained:
+                            Log.i("VirtualHome", jsonObject.toString());
+
+                            //Saving the userID in shared preferences:
+
+                            Log.i("Login ", "User ID: " + userID);
+
+
+                            //Save email id, password and user id.
+                            SharedPreferences settings = getSharedPreferences(PREFERENCES_Gallery_FILE_NAME, 0);
+
+                            //Initially for cross checking
+                            System.out.println(settings.getString("user_id", "0"));
+                            System.out.println(settings.getString("email", "0"));
+                            System.out.println(settings.getString("password", "0"));
+
+
+                            SharedPreferences.Editor editor = settings.edit();
+                            editor.putString("user_id", userID);
+                            editor.putString("email", email);
+                            editor.putString("password", pwd);
+
+                            //saving all the preferences
+                            editor.putInt("gender", Integer.parseInt(gender)) ;
+                            editor.putInt("family", Integer.parseInt(family));
+                            editor.putInt("profession", Integer.parseInt(profession));
+                            editor.putBoolean("gardening", Boolean.parseBoolean(gardening));
+                            editor.putBoolean("interiorDesign", Boolean.parseBoolean(interiorDesign));
+                            editor.putBoolean("cooking", Boolean.parseBoolean(cooking));
+                            editor.putBoolean("painting", Boolean.parseBoolean(painting));
+                            editor.putBoolean("reading", Boolean.parseBoolean(reading));
+                            editor.putBoolean("music", Boolean.parseBoolean(music));
+
+
+
+                            editor.commit();
+
+                            //For confirmation
+                            System.out.println(settings.getString("user_id", "0"));
+                            System.out.println(settings.getString("email", "0"));
+                            System.out.println(settings.getString("password", "0"));
+
+                            //Save the preferences data
+                            //Get the data and save the preferences.
+
+
+
+
+                        }
+
+
+                    } catch (JSONException ex) {
+                        Log.i("VirtualHome-Gallery", " caught JSON exception");
+                        ex.printStackTrace();
+                        return null;
+                    }
+
+
+                }
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+
+            return "";
+
+        }
+
+
+        protected void onPostExecute(String result) {
+            Log.i("VirtualHome-Login", "onPostExecute");
+
+
+            if(!LoginSuccess)
+            {
+                Toast.makeText(getApplicationContext(), "Invalid credentials!", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                //If returning user, navigating the to the gallery
+                Intent intent = new Intent(Login.this, SofaGallery.class);
+                startActivity(intent);
+            }
+
+        }
+    }
+
+
+
+
+
 }
 
--- ARHome/app/src/main/java/com/wikitude/virtualhome/Preferences.java
+++ ARHome/app/src/main/java/com/wikitude/virtualhome/Preferences.java
@@ -1,46 +1,8 @@
 package com.wikitude.virtualhome;
 
-
-/*import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
-public class Preferences extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preferences);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_preferences, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}*/
-
-
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -67,63 +29,188 @@ public class Preferences extends Activity {
     String profession = null;
     boolean chstate1,chstate2,chstate3,chstate4,chstate5,chstate6;
     JSONObject userPrefJson;
-    String userEmail = null;
+    String newUserFlag = null;
+    boolean noUserID=false;
+    public static final String PREFERENCES_Gallery_FILE_NAME = "VHGalleryPreferences";
+    String userID =null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //getting no userID status:
+        SharedPreferences settings = getSharedPreferences(PREFERENCES_Gallery_FILE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+
+
+        userID = settings.getString("user_id", "-1");
+       /* //For checking:
+        userID = "-1";*/
+
+
+        Log.i("Pref", "UserIDStatus: " + userID);
+
+        //Obtaining the new user flag status flg status
+        newUserFlag = getIntent().getStringExtra("newUserFlag");
+
+      /*  //Just for checking:
+        newUserFlag="false";*/
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
-
-        //Obtaining the email id
-        userEmail = getIntent().getStringExtra("email");
-
-
-        // Spinner element
-
-        spinner1 = (Spinner) findViewById(R.id.FamilyType);
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
-                R.array.familytypelist, R.layout.spinner_style_preferences );
-        // Specify the layout to use when the list of choices appears
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner1.setAdapter(adapter1);
-
-        spinner2 = (Spinner) findViewById(R.id.SexType1);
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
-                R.array.sextypelist, R.layout.spinner_style_preferences );
-        // Specify the layout to use when the list of choices appears
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner2.setAdapter(adapter2);
-
-
-        spinner3 = (Spinner) findViewById(R.id.ProfessionType);
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
-                R.array.professiontypelist, R.layout.spinner_style_preferences );
-        // Specify the layout to use when the list of choices appears
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner3.setAdapter(adapter3);
-
-
-
         Button button = (Button) findViewById(R.id.button_submit_preferences);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                onSubmit();
+
+        checkbox1= (CheckBox) findViewById(R.id.checkbox_hobby1); //gardening
+
+        checkbox2= (CheckBox) findViewById(R.id.checkbox_hobby2); //interiorDesign
+
+        checkbox3= (CheckBox) findViewById(R.id.checkbox_hobby3); //cooking
+
+        checkbox4= (CheckBox) findViewById(R.id.checkbox_hobby4); //painting
+
+        checkbox5= (CheckBox) findViewById(R.id.checkbox_hobby5); //reading
+
+        checkbox6= (CheckBox) findViewById(R.id.checkbox_hobby6); //music
+
+
+        //getting the preferences
+
+        if (userID.equals("-1")) {
+            noUserID = true;
+            Toast.makeText(getApplicationContext(), "You have not logged in! Please login to set preferences", Toast.LENGTH_SHORT).show();
+            button.setEnabled(false);
+            //to confirm
+            Log.i("button disabled: ", button.isEnabled() + "");
+
+            //Making the newUserFlag set as true, so that the the page displays but with the submit button disabled.
+            newUserFlag="true";
+
+        }
+
+
+        //Disabling the button if no user has logged
+
+
+        if (newUserFlag != null) {
+            if (newUserFlag.equals("true")) {
+
+                Log.i("pref:", "inside new user");
+
+                // Spinner element
+                spinner1 = (Spinner) findViewById(R.id.FamilyType);
+                ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                        R.array.familytypelist, R.layout.spinner_style_preferences);
+                // Specify the layout to use when the list of choices appears
+                adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                // Apply the adapter to the spinner
+                spinner1.setAdapter(adapter1);
+
+                spinner2 = (Spinner) findViewById(R.id.SexType1);
+                ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                        R.array.sextypelist, R.layout.spinner_style_preferences);
+                // Specify the layout to use when the list of choices appears
+                adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                // Apply the adapter to the spinner
+                spinner2.setAdapter(adapter2);
+
+
+
+                spinner3 = (Spinner) findViewById(R.id.ProfessionType);
+                ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                        R.array.professiontypelist, R.layout.spinner_style_preferences);
+                // Specify the layout to use when the list of choices appears
+                adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                // Apply the adapter to the spinner
+                spinner3.setAdapter(adapter3);
+
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Perform action on click
+                        onSubmit();
+                    }
+                });
+
+
+            } else {
+                //getting the preferences:
+
+                int gender=settings.getInt("gender",-1);
+                int family=settings.getInt("family",-1);
+                int profession=settings.getInt("profession",-1);
+
+                boolean gardening=settings.getBoolean("gardening", true);
+                boolean interiorDesign=settings.getBoolean("interiorDesign",false);
+                boolean cooking=settings.getBoolean("cooking",false);
+                boolean painting=settings.getBoolean("painting",false);
+                boolean reading=settings.getBoolean("reading",false);
+                boolean music=settings.getBoolean("music",false);
+
+
+
+
+
+                Log.i("pref:", "inside returning user");
+
+                Log.i("pref:", "gender"+gender+" family"+family+"profession"+profession);
+
+
+                if(gender!=-1&&family!=-1&&profession!=-1)
+                {
+                    // Spinner element
+                    spinner1 = (Spinner) findViewById(R.id.FamilyType);
+                    ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                            R.array.familytypelist, R.layout.spinner_style_preferences);
+                    // Specify the layout to use when the list of choices appears
+                    adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    // Apply the adapter to the spinner
+                    spinner1.setAdapter(adapter1);
+                    spinner1.setSelection(family);
+
+                    spinner2 = (Spinner) findViewById(R.id.SexType1);
+                    ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                            R.array.sextypelist, R.layout.spinner_style_preferences);
+                    // Specify the layout to use when the list of choices appears
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    // Apply the adapter to the spinner
+                    spinner2.setAdapter(adapter2);
+                    spinner2.setSelection(gender);
+
+
+                    spinner3 = (Spinner) findViewById(R.id.ProfessionType);
+                    ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                            R.array.professiontypelist, R.layout.spinner_style_preferences);
+                    // Specify the layout to use when the list of choices appears
+                    adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    // Apply the adapter to the spinner
+                    spinner3.setAdapter(adapter3);
+                    spinner3.setSelection(profession);
+
+                }
+
+                checkbox1.setChecked(gardening);
+                checkbox2.setChecked(interiorDesign);
+                checkbox3.setChecked(cooking);
+                checkbox4.setChecked(painting);
+                checkbox5.setChecked(reading);
+                checkbox6.setChecked(music);
+
+
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Perform action on click
+                        onSubmit();
+                    }
+                });
+
+
+
             }
-        });
 
-
-
-
-
-
+        }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -157,6 +244,10 @@ public class Preferences extends Activity {
         gender = String.valueOf(spinner2.getSelectedItem());
         profession = String.valueOf(spinner3.getSelectedItem());
 
+        int familyIndex=spinner1.getSelectedItemPosition();
+        int genderIndex=spinner2.getSelectedItemPosition();
+        int professionIndex=spinner3.getSelectedItemPosition();
+
         checkbox1= (CheckBox) findViewById(R.id.checkbox_hobby1); //gardening
         chstate1=checkbox1.isChecked();
 
@@ -175,19 +266,47 @@ public class Preferences extends Activity {
         checkbox6= (CheckBox) findViewById(R.id.checkbox_hobby6); //music
         chstate6=checkbox6.isChecked();
 
-        String result = family + " " + gender + " " + profession + " " + "1:" + chstate1 + "2:" + chstate2 + "3:" + chstate3 + "4:" + chstate4 + "5:" + chstate5 + "6:" + chstate6;
+        String result = family + " " + gender + " " + profession + " " + "1:" + chstate1 + "2:" + chstate2 + "3:" + chstate3 + "4:" + chstate4 + "5:" + chstate5 + "6:" + chstate6 + "x:" + familyIndex+ "y:" + genderIndex+ "z:" + professionIndex;
+        Log.i("pref", result);
+
+        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
 
 
-        //Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+/*
+        //Create preferences json
+        createUserJson();*/
 
-        //Add code to insert data into preferences table in database.
 
+        //CALL the database here:
+
+        //Save the features to local
+        SharedPreferences settings = getSharedPreferences(PREFERENCES_Gallery_FILE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+
+
+        editor.putInt("gender", genderIndex) ;
+        editor.putInt("family", familyIndex);
+        editor.putInt("profession", professionIndex);
+        editor.putBoolean("gardening", chstate1);
+        editor.putBoolean("interiorDesign", chstate2);
+        editor.putBoolean("cooking", chstate3);
+        editor.putBoolean("painting", chstate4);
+        editor.putBoolean("reading", chstate5);
+        editor.putBoolean("music", chstate6);
+
+        editor.commit();
+
+//For now: keeping the call here
         //Create preferences json
         createUserJson();
 
-
         //Launching the gallery after data insertion
         launchGallery();
+
+
+
+
+
 
 
     }
@@ -209,7 +328,7 @@ public class Preferences extends Activity {
         try {
 
             //userEmail
-            userPrefJson.put("email", userEmail);
+
 
             userPrefJson.put("gender", gender);
             userPrefJson.put("family", family);
