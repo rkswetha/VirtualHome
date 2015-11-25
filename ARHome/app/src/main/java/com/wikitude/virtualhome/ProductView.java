cda7c3697249c58c8996eaf7a8b0ed94e10a47e0
RadhikaSNM <radsnm@gmail.com>
2015-11-25 01:12:29 -0800

    User transaction support- UI

--- ARHome/app/src/main/java/com/wikitude/virtualhome/ProductView.java
+++ ARHome/app/src/main/java/com/wikitude/virtualhome/ProductView.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,11 +20,30 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class ProductView extends Activity {
 
     String markerPresent = null;
     String location = null;
     String productID = null;
+    String userIDStr = null;
+    public static final String PREFERENCES_Gallery_FILE_NAME = "VHGalleryPreferences";
+    String ConstantURL = URLAPIConstant.URL;
+    JSONObject userPrefJson;
+    boolean galleryJsonStreamSuccess;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,18 +88,176 @@ public class ProductView extends Activity {
         TextView descriptionTextView = (TextView) findViewById(R.id.galleryItemDescription);
         descriptionTextView.setText(description);
 
+        //Get the user id: If logged in user then update user transactions else
+
+        //getting no userID status:
+        SharedPreferences settings = getSharedPreferences(PREFERENCES_Gallery_FILE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+         userIDStr = settings.getString("user_id", "-1");
+        Log.i("Pref", "UserID: " + userIDStr);
 
 
-        //On text click:
+
+        //update transactions only for the logged in user
         imageView.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                openARView();
+                if(userIDStr == "-1")
+                openARView(null);
+                else
+                    getUserTransactions();
             }
         });
+		
+    }
 
 
+    public void getUserTransactions() {
+
+        SharedPreferences settings = getSharedPreferences(PREFERENCES_Gallery_FILE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        int userID = Integer.parseInt(userIDStr);
+        userPrefJson = new JSONObject();
+        try {
+
+            Log.i("VirtualHome-ProductView", "create json user transaction");
+            userPrefJson.put("user_id", userID);
+            userPrefJson.put("product_id", productID);
+
+
+        } catch (JSONException e) {
+            Log.i("VirtualHome-ProductView", "Exception in Json creation");
+            e.printStackTrace();
+        }
+
+        Log.i("VirtualHome-ProductView", "JSON User preference input:" + userPrefJson.toString());
+
+        new UserTransactionAsynTask().execute();
 
     }
+
+
+    private class UserTransactionAsynTask extends AsyncTask<String, String, String> {
+
+        private String[] imageLocations;
+        JSONArray queryArray;
+
+        protected String doInBackground(String... arg0) {
+            HttpURLConnection urlConnection = null;
+            Log.i("VirtualHome-ProductView", "Inside user transactions");
+            //int userID = Integer.parseInt(userIDStr);
+            String url1 = ConstantURL + "usertransactions/"+userIDStr;
+
+            StringBuilder sb = new StringBuilder();
+            try {
+
+                URL url = new URL(url1);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestMethod("PUT");
+                urlConnection.setUseCaches(false);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+
+                //urlConnection.setRequestProperty("Host", "android.schoolportal.gr");
+                urlConnection.connect();
+
+                OutputStreamWriter out = new OutputStreamWriter(urlConnection.getOutputStream());
+                out.write(userPrefJson.toString());
+                out.flush();
+                out.close();
+
+                String responseMessage = urlConnection.getResponseMessage();
+                int HttpResult = urlConnection.getResponseCode();
+                System.out.println("CODE: " + HttpResult);
+
+                //TODO: check for created or check for validated. If not throw pop up as error.
+                System.out.println(responseMessage);
+
+                if (urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+
+                    Log.i("VirtualHome:AR", "---Failed : HTTP error code : "
+                            + urlConnection.getResponseCode());
+                    galleryJsonStreamSuccess = false;
+
+                } else {
+                    //It is verified:
+                    galleryJsonStreamSuccess = true;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(
+                            (urlConnection.getInputStream())));
+
+                    String serverOutput;
+                    StringBuilder serverSB = new StringBuilder();
+                    System.out.println("Output from Server .... \n");
+                    while ((serverOutput = br.readLine()) != null) {
+
+                        System.out.println(serverOutput);
+                        serverSB.append(serverOutput + "\n");
+
+                    }
+
+                    JSONObject jsonObject = null;
+
+                    try {
+                        Log.i("VirtualHome:AR ", "Retrieve jsonContent");
+
+                        jsonObject = new JSONObject(serverSB.toString());
+                        System.out.println("Full json object: " + jsonObject.toString());
+
+                        queryArray = jsonObject.getJSONArray("results");
+                        int resultSize = queryArray.length();
+
+                        Log.i("VirtualHome:AR", "resultSize:" + resultSize);
+                        Log.i("VirtualHome:AR", "queryArray:" + queryArray);
+
+                        imageLocations = new String[resultSize];
+
+                        for (int i = 0; i < queryArray.length(); i++) {
+                            JSONObject jsonAttributes = queryArray.getJSONObject(i);
+                            imageLocations[i] = jsonAttributes.getString("url");
+                        }
+
+                    } catch (JSONException e) {
+                        Log.i("VirtualHome:AR", " caught JSON exception");
+                        e.printStackTrace();
+                        return null;
+                    }
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+
+            return "";
+
+        }
+
+        protected void onPostExecute(String result) {
+            Log.i("VirtualHome:AR", "onPostExecute-Getting POST reply: result"+result);
+
+            if (!galleryJsonStreamSuccess) {
+                Toast.makeText(getApplicationContext(), "Updating transactions failed", Toast.LENGTH_SHORT).show();
+            } else {
+
+                for (int i = 0; i < imageLocations.length; i++) {
+                    Log.i("VirtualHome-ProductView", "onPostExecute:URL obtained"+imageLocations[i]);
+                }
+                openARView(imageLocations);
+
+            }
+
+
+        }
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,8 +282,11 @@ public class ProductView extends Activity {
     }
 
 
-    public void openARView()
+    public void openARView(String[] imageLocations)
     {
+
+        //If null
+        final String[] images = imageLocations;
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
 
         // set title
@@ -127,6 +310,9 @@ public class ProductView extends Activity {
                             theIntent.putExtra("MarkerPresent", "YES");
                             theIntent.putExtra("ImagePath", location);
                             theIntent.putExtra("productid",productID);
+                            theIntent.putExtra("PastTransaction",images );
+                            Log.i("images intent", images.toString());
+                            Log.i("images intent",images[0]);
                             startActivity(theIntent);
 
                         } catch (Exception e) {
@@ -150,6 +336,8 @@ public class ProductView extends Activity {
                             theIntent.putExtra("MarkerPresent", "NO");
                             theIntent.putExtra("ImagePath", location);
                             theIntent.putExtra("productid",productID);
+                            theIntent.putExtra("PastTransaction",images );
+                            Log.i("images intent", images[0]);
                             startActivity(theIntent);
                         } catch (Exception e) {
                             Toast.makeText(getApplicationContext(), "\n className not defined/accessible",
